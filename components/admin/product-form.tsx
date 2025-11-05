cb401474936949d9e1d38d5f81a383208f98ac3d
1t0t0 <totophandolack915@gmail.com>
2025-11-05 22:47:29 +0700

    Add Image Uploads

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -14,6 +14,9 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 import { createProduct, updateProduct } from "@/lib/actions/product.actions";
+import { UploadButton } from "@uploadthing/react";
+import { Card, CardContent } from "../ui/card";
+import Image from "next/image";
 
 const ProductForm = ({type, product, productId}: {
     type: 'Create' | 'Update';
@@ -60,6 +63,8 @@ const onSubmit:SubmitHandler<z.infer<typeof insertProductSchema>> = async (value
     }
 
 }
+
+const images = form.watch('images');
 
 
     return ( 
@@ -178,6 +183,37 @@ const onSubmit:SubmitHandler<z.infer<typeof insertProductSchema>> = async (value
 
         <div className="upload-field flex flex-col md:flex-row gap-5">
             {/* Images */}
+            <FormField
+            control={form.control}
+            name='images'
+            render={() => (
+                <FormItem className="w-full">
+                    <FormLabel>Images</FormLabel>
+                    <Card>
+                        <CardContent className="space-y-2 mt-2 min-h-48">
+                            <div className="flex-start space-x-2">
+                                {  images.map((image: string) => (
+                                    <Image key={image} src={image} alt="product image" className="w-20 h-20 object-cover object-center rounded-sm" width={100} height={100}/>
+                                )) }
+                                <FormControl>
+                                    <UploadButton endpoint='imageUploader' onClientUploadComplete={(res: {url: string}[]) => {
+                                        form.setValue('images', [...images, res[0].url])
+                                    }}
+                                    onUploadError={(error: Error) => {
+                                        toast.error(`ERROR! ${error.message}`)
+
+                                    }}
+                                    />
+                                </FormControl>
+                            </div>
+                        </CardContent>
+                    </Card>
+
+                    <FormMessage/>
+
+                </FormItem>
+            )}
+            />
         </div>
 
         <div className="upload-field">
