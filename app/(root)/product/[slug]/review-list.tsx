396eed852c3247bec80c67fab5708fa2f32497b0
1t0t0 <totophandolack915@gmail.com>
2025-11-10 08:23:44 +0700

    Review form dialog components

--- app/(root)/product/[slug]/review-list.tsx
+++ app/(root)/product/[slug]/review-list.tsx
@@ -3,6 +3,7 @@
 import { Review } from "@prisma/client";
 import Link from "next/link";
 import { useState } from "react";
+import ReviewForm from "./review-form";
 
 const ReviewList = ({
     userId,
@@ -20,9 +21,7 @@ const ReviewList = ({
         {reviews.length === 0 && <div>No reviews yet</div>}
         {
             userId ? (
-                <>
-                {/* Review from here */}
-                </>
+                <ReviewForm userId={userId} productId={productId} />
             ): (
                 <div>
                     Please <Link className="text-blue-700 px-2" href={`/sign-in?callbackUrl=/product${productSlug}`}>
