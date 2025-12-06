99341c99dfec0089f8a0db36d582eb832d8f1d0c
1t0t0 <totophandolack915@gmail.com>
2025-12-06 23:34:33 +0700

    Update index

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'EcomerceStore';
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A modern e-commerce platform built with Next.js and TypeScript.';
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'https://shop.ttoto.online/';
 
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 6
 
