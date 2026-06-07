23355c0b943b15e6c9342aca272b71c1d0ca2496
Ttoto Phandolack <totophandolack915@gmail.com>
2026-06-08 00:22:24 +0700

    Fix bug

--- app/(root)/order/[id]/stripe-payment-success/page.tsx
+++ app/(root)/order/[id]/stripe-payment-success/page.tsx
@@ -4,7 +4,6 @@ import { notFound, redirect } from 'next/navigation';
 import Stripe from 'stripe';
 import { getOrderById } from '@/lib/actions/order.actions';
 import { Button } from '@/components/ui/button';
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 export const metadata: Metadata = {
   title: 'Stripe Payment Success',
 };
@@ -12,6 +11,7 @@ const SuccessPage = async (props: {
   params: Promise<{ id: string }>;
   searchParams: Promise<{ payment_intent: string }>;
 }) => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
   //  Get the order id and payment intent id from the URL
   const { id } = await props.params;
   const { payment_intent: paymentIntentId } = await props.searchParams;
